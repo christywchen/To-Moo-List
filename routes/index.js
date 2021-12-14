1d2e0887b26995881acf5706818df35c5dbf9c64
Christy Chen <christy.w.chen@gmail.com>
2021-12-14 11:18:34 -0800

    added some pug templates for the dashboard.

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   console.log(res.locals);
-  res.render('index', { title: 'Home' });
+  res.render('dashboard-list', { title: 'Home' });
 });
 
 module.exports = router;
