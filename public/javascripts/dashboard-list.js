ec1048be1a90834e38140ca9c940c64b7250b9b9
jstnswn <jstnswn@gmail.com>
2021-12-15 09:02:19 -0800

    ready for merge

--- public/javascripts/dashboard-list.js
+++ public/javascripts/dashboard-list.js
@@ -7,7 +7,7 @@ const initializePage = async () => {
         const stateId = { id: "100" };
         const taskRes = await fetch(`/api/lists/${e.target.className}/tasks`)
         const { tasks } = await taskRes.json();
-        const taskContainer = document.getElementById("taskContainer");
+        const taskContainer = document.getElementById("tasksContainer");
         tasks.forEach(task => {
             const div = document.createElement("div");
             div.innerHTML = createTaskHtml(task.name);
@@ -40,13 +40,14 @@ function createTaskHtml(taskName) {
 
 const createTask = async (e) => {
     e.preventDefault();
-    const taskData = document.querySelector('#add-task-form');
-    const taskContainer = document.getElementById("taskContainer");
+    const taskData = document.querySelector('#add-task-input');
+    const taskContainer = document.getElementById("tasksContainer");
     const formData = new FormData(taskData);
     const name = formData.get('name');
     const body = { name, listId };
     const div = document.createElement('div');
     const input = document.getElementById('name');
+    div.classList.add('task')
     if (input.value.length) {
         div.innerHTML = createTaskHtml(name);
         taskContainer.appendChild(div);
@@ -64,28 +65,55 @@ const createTask = async (e) => {
     }
 };
 
-const submitDiv = document.querySelector('#add-task-form');
-const addTask  = document.querySelector('.addTaskButton');
-const taskDiv = document.querySelector('.addTaskDiv');
+// const submitDiv = document.querySelector('#add-task-form');
+// const addTask  = document.querySelector('.addTaskButton');
+// const taskDiv = document.querySelector('.addTaskDiv');
 
-const showButton = (e) => {
-    addTask.hidden = false;
-    taskDiv.hidden = false;
-    console.log(taskDiv.hidden, addTask.hidden)
-};
-const hideButton = (e) => {
-    e.stopPropagation()
-    if (e.target.id !== ('name' || 'taskButton')) {
-        addTask.hidden = true;
-        taskDiv.hidden = true;
-    }
-};
+const addTaskFormDiv = document.querySelector('#add-task-form');
+const addTaskInp = document.querySelector('input#name.inp-field');
+const addTaskButtonDiv = document.querySelector('.add-task-button');
+const addTaskButton = document.querySelector('.add-task-button button');
+
+
+// const showButton = (e) => {
+//     addTask.hidden = false;
+//     taskDiv.hidden = false;
+//     console.log(taskDiv.hidden, addTask.hidden)
+// };
+// const hideButton = (e) => {
+//     e.stopPropagation()
+//     if (e.target.id !== ('name' || 'taskButton')) {
+//         addTask.hidden = true;
+//         taskDiv.hidden = true;
+//     }
+// };
 
 
 // Load events
 window.addEventListener("load", async (event) => {
     initializePage();
-    addTask.addEventListener('click', createTask);
-    submitDiv.addEventListener('click', showButton);
-    document.addEventListener('click', hideButton);
+    addTaskButton.addEventListener('click', createTask);
+    // submitDiv.addEventListener('click', showButton);
+    // document.addEventListener('click', hideButton);
+    document.addEventListener('click', (e) => {
+        // show submit button if event target is a descendent of addTaskFormDiv
+        // otherwise, do not show the submit button
+        e.preventDefault()
+        if (addTaskFormDiv.contains(e.target)) {
+            addTaskButtonDiv.classList.add('add-task-button-transition');
+        }
+        else {
+            addTaskButtonDiv.classList.remove('add-task-button-transition');
+        }
+    });
+
+    addTaskInp.addEventListener('keyup', (e) => {
+        e.preventDefault()
+        if (addTaskInp.value) {
+            addTaskButton.disabled = false;
+        } else {
+            addTaskButton.disabled = true;
+        }
+    });
+
 });
